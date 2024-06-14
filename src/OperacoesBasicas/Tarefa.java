5572a8764a429169e4440f2ac2a096605d023d7d
Vyctor Rodrigues <vyctorqueen@gmail.com>
2024-06-14 20:53:13 -0300

    meu projeto List dIo

---
+++ src/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,23 @@
+package OperacoesBasicas;
+
+public class Tarefa {
+    //attibuto
+
+    private String descricao;
+
+
+    //contrutor que recebe descricao e criou um objeto do tipo tarefa
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+    //criei um metodo getDescricao como o atributo ta privado e eu vou querer saber a descricao da tarefa
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    public String toString() {
+        return descricao;
+    }
+
+    
+}
